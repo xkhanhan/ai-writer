cb32b0c67f26a8ed11a7bc4685bfb0093e7797e4
xkhanhan <1427742566@qq.com>
2026-07-09 23:24:56 +0800

    feat(panel-container): add PanelGroup, PanelContainer, CSS and barrel exports

---
+++ shared/ui/panel-container/index.tsx
@@ -0,0 +1,5 @@
+export { PanelContainer } from "./panel-container";
+export { PanelGroup } from "./panel-group";
+export { Panel } from "./panel";
+export { Divider } from "./divider";
+export type { Direction, PanelState, PanelLayout } from "./types";
